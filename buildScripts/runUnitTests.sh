b1359ebc59286360f3f72eb6d12333a7048a4769
Brandon Alexander <brandon@whilethis.com>
2010-07-06 16:02:37 -0400

    Adding static analysis to the CI build

--- buildScripts/runUnitTests.sh
+++ buildScripts/runUnitTests.sh
@@ -9,6 +9,6 @@ set -ex
 
 . "$WORKSPACE/buildScripts/build.config"
 
-xcodebuild -configuration $CONFIGURATION -sdk $SDK clean;
-xcodebuild -configuration "Debug" -sdk $SIMULATOR -target LogicTests | /usr/local/bin/ocunit2junit.rb;
+scan-build xcodebuild -configuration $CONFIGURATION -sdk $SDK clean GCC_GENERATE_TEST_COVERAGE_FILES=YES PREBINDING=NO GCC_OPTIMIZATION_LEVEL=0;
+scan-build -o html xcodebuild -configuration "Debug" -sdk $SIMULATOR -target LogicTests GCC_GENERATE_TEST_COVERAGE_FILES=YES PREBINDING=YES GCC_OPTIMIZATION_LEVEL=0 DEAD_CODE_STRIPPING=YES GCC_DEBUGGING_SYMBOLS=full GENERATE_PROFILING_CODE=YES GCC_INSTRUMENT_PROGRAM_FLOW_ARCS=YES OTHER_LDFLAGS=" -L//Developer/Platforms/iPhoneSimulator.platform/Developer/SDKs/iPhoneSimulator3.0.sdk/usr/lib/gcc/i686-apple-darwin10/4.2.1/ -fprofile-arcs -lgcov -prebind "| /usr/local/bin/ocunit2junit.rb;
 

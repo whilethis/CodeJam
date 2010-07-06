6d934862e361433aca50a71691ea5af692b077dd
Brandon Alexander <brandon@whilethis.com>
2010-07-06 13:44:24 -0400

    More build script fixes

--- buildScripts/build.sh
+++ buildScripts/build.sh
@@ -27,7 +27,7 @@ for sdk in $SDKS; do
         cert="$WORKSPACE/build/$provision"
         archive="$OUTPUT/$JOB_NAME-$BUILD_NUMBER-$config.zip";
         [ -f "$cert" ] && cp "$cert" "$PROFILE_HOME"
-        cd $WORKSPACE/iphone
+#        cd $WORKSPACE/iphone
 
         xcodebuild -configuration $config -sdk $sdk clean;
         xcodebuild -configuration $config -sdk $sdk || failed build;

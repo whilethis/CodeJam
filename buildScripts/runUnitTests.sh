dfa05e68cd22955b5bf5fd3bf3b7332a767242fd
Brandon Alexander <brandon@whilethis.com>
2010-07-06 17:32:19 -0400

    Adding a custom configuration to the Xcode build for doing code coverage.

--- buildScripts/runUnitTests.sh
+++ buildScripts/runUnitTests.sh
@@ -9,6 +9,6 @@ set -ex
 
 . "$WORKSPACE/buildScripts/build.config"
 
-xcodebuild -configuration "Debug" -sdk $SIMULATOR clean
-xcodebuild -configuration "Debug" -sdk $SIMULATOR -target LogicTests | /usr/local/bin/ocunit2junit.rb;
+xcodebuild -configuration "Debug-GCov" -sdk $SIMULATOR clean
+xcodebuild -configuration "Debug-GCov" -sdk $SIMULATOR -target LogicTests | /usr/local/bin/ocunit2junit.rb;
 

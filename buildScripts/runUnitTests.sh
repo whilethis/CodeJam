fe87fde273eb6d447e7a9ca1efbcb513d4025b1e
Brandon Alexander <brandon@whilethis.com>
2010-07-06 16:48:27 -0400

    Adding the scan-build to the clean phase for kicks

--- buildScripts/runUnitTests.sh
+++ buildScripts/runUnitTests.sh
@@ -9,6 +9,6 @@ set -ex
 
 . "$WORKSPACE/buildScripts/build.config"
 
-xcodebuild -configuration "Debug" -sdk $SDK clean GCC_GENERATE_TEST_COVERAGE_FILES=YES PREBINDING=NO GCC_OPTIMIZATION_LEVEL=0;
+/usr/local/checker/scan-build xcodebuild -configuration "Debug" -sdk $SIMULATOR clean GCC_GENERATE_TEST_COVERAGE_FILES=YES PREBINDING=NO GCC_OPTIMIZATION_LEVEL=0;
 /usr/local/checker/scan-build -o html xcodebuild -configuration "Debug" -sdk $SIMULATOR -target LogicTests GCC_GENERATE_TEST_COVERAGE_FILES=YES PREBINDING=YES GCC_OPTIMIZATION_LEVEL=0 DEAD_CODE_STRIPPING=YES | /usr/local/bin/ocunit2junit.rb;
 

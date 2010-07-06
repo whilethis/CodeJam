3f2a54a99f97608e776b83a24209c99f1aceaa57
Brandon Alexander <brandon@whilethis.com>
2010-07-06 16:39:12 -0400

    Removing some xcodebuild options

--- buildScripts/runUnitTests.sh
+++ buildScripts/runUnitTests.sh
@@ -10,5 +10,5 @@ set -ex
 . "$WORKSPACE/buildScripts/build.config"
 
 xcodebuild -configuration "Debug" -sdk $SDK clean GCC_GENERATE_TEST_COVERAGE_FILES=YES PREBINDING=NO GCC_OPTIMIZATION_LEVEL=0;
-xcodebuild -configuration "Debug" -sdk $SIMULATOR -target LogicTests GCC_GENERATE_TEST_COVERAGE_FILES=YES PREBINDING=YES GCC_OPTIMIZATION_LEVEL=0 DEAD_CODE_STRIPPING=YES GCC_DEBUGGING_SYMBOLS=full GENERATE_PROFILING_CODE=YES GCC_INSTRUMENT_PROGRAM_FLOW_ARCS=YES OTHER_LDFLAGS=" -L/$SIMULATOR/usr/lib/gcc/i686-apple-darwin10/4.2.1/ -fprofile-arcs -lgcov -prebind " | /usr/local/bin/ocunit2junit.rb;
+xcodebuild -configuration "Debug" -sdk $SIMULATOR -target LogicTests GCC_GENERATE_TEST_COVERAGE_FILES=YES PREBINDING=YES GCC_OPTIMIZATION_LEVEL=0 DEAD_CODE_STRIPPING=YES | /usr/local/bin/ocunit2junit.rb;
 

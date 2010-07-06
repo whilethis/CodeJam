3dc789ac7eff89de12d79c1546010784f44afa65
Brandon Alexander <brandon@whilethis.com>
2010-07-06 16:35:19 -0400

    Modifying build script to use the simulator variable everywhere

--- buildScripts/runUnitTests.sh
+++ buildScripts/runUnitTests.sh
@@ -9,6 +9,6 @@ set -ex
 
 . "$WORKSPACE/buildScripts/build.config"
 
-/usr/local/checker/scan-build xcodebuild -configuration "Debug" -sdk $SDK clean GCC_GENERATE_TEST_COVERAGE_FILES=YES PREBINDING=NO GCC_OPTIMIZATION_LEVEL=0;
-/usr/local/checker/scan-build -o html xcodebuild -configuration "Debug" -sdk $SIMULATOR -target LogicTests GCC_GENERATE_TEST_COVERAGE_FILES=YES PREBINDING=YES GCC_OPTIMIZATION_LEVEL=0 DEAD_CODE_STRIPPING=YES GCC_DEBUGGING_SYMBOLS=full GENERATE_PROFILING_CODE=YES GCC_INSTRUMENT_PROGRAM_FLOW_ARCS=YES OTHER_LDFLAGS=" -L/$SIMULATOR/usr/lib/gcc/i686-apple-darwin10/4.2.1/ -fprofile-arcs -lgcov -prebind " | /usr/local/bin/ocunit2junit.rb;
+xcodebuild -configuration "Debug" -sdk $SDK clean GCC_GENERATE_TEST_COVERAGE_FILES=YES PREBINDING=NO GCC_OPTIMIZATION_LEVEL=0;
+xcodebuild -configuration "Debug" -sdk $SIMULATOR -target LogicTests GCC_GENERATE_TEST_COVERAGE_FILES=YES PREBINDING=YES GCC_OPTIMIZATION_LEVEL=0 DEAD_CODE_STRIPPING=YES GCC_DEBUGGING_SYMBOLS=full GENERATE_PROFILING_CODE=YES GCC_INSTRUMENT_PROGRAM_FLOW_ARCS=YES OTHER_LDFLAGS=" -L/$SIMULATOR/usr/lib/gcc/i686-apple-darwin10/4.2.1/ -fprofile-arcs -lgcov -prebind " | /usr/local/bin/ocunit2junit.rb;
 
